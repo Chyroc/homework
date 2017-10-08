389785d6aef2687e5713b95e11388a72d8cb760b
Chyroc <chen_yunpeng@foxmail.com>
2017-10-08 22:05:56 +0800

    fix 5-2

--- 5/2.py
+++ 5/2.py
@@ -1,7 +1,13 @@
 def check_prime_number(x):
+    if x == 1:
+        return False
+    elif x == 2:
+        return True
+
     for i in range(2, x):
         if x % i == 0:
             return False
+
     return True
 
 
