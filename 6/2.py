1d3a70c7f4029211ffa400a31f6bfc9963595500
Chyroc <chen_yunpeng@foxmail.com>
2017-10-15 14:00:56 +0800

    update 6-2

--- 6/2.py
+++ 6/2.py
@@ -14,6 +14,19 @@ def substrfun(s):
     return smax
 
 
+def substrfun2(s):
+    smax = ''
+    ssub = s[0]
+    for index in range(1, len(s) - 1):
+        if s[index - 1] <= s[index]:
+            ssub += s[index]
+        else:
+            if len(ssub) > len(smax):
+                (ssub, smax) = (smax, ssub)
+            ssub = s[index]
+    return smax
+
+
 if __name__ == '__main__':
     print(substrfun('abcbcd') == 'abc')
     print(substrfun('azcbobobegghakl') == 'beggh')
